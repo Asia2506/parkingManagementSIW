24a57b1cd510d2dab94b03318ed7f6c290f4cc53
ASIA FULVI <asi.fulvi@stud.uniroma3.it>
2025-04-17 13:07:37 +0200

    Progetto vuoto

---
+++ src/main/java/siw/uniroma3/parkingManagementSIW/ParkingManagementSiwApplication.java
@@ -0,0 +1,13 @@
+package siw.uniroma3.parkingManagementSIW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagementSiwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagementSiwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/siw/uniroma3/parkingManagementSIW/ParkingManagementSiwApplicationTests.java
@@ -0,0 +1,13 @@
+package siw.uniroma3.parkingManagementSIW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementSiwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
