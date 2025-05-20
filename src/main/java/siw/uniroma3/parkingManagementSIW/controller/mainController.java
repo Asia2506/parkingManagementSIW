effe65c886d2ca6fb8de9568897c0a88fc7a7db6
isabelladolci <tuo@email.com>
2025-05-20 13:54:47 +0200

    creazione mainController

---
+++ src/main/java/siw/uniroma3/parkingManagementSIW/controller/mainController.java
@@ -0,0 +1,13 @@
+package siw.uniroma3.parkingManagementSIW.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainController {
+		
+		@GetMapping("/")
+		public String index() {
+			return "home.html";	
+		}
+}
